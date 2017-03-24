1149589fa38a2429c70620cd02abc5c9e144dc63
Remco <caseda08@hotmail.com>
2017-03-24 18:40:30 +0100

    merge #1, minor coding consitency, bump version v1.1.2

--- drivers/powernode-6/driver.js
+++ drivers/powernode-6/driver.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const ZwaveDriver = require('homey-zwavedriver');
-const pollInterval = {};
+const endpointInterval = {};
 
 // http://www.pepper1.net/zwavedb/device/280
 
@@ -67,13 +67,13 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			module.exports.getSettings(node.device_data, (err, settings) => {
 				if (err) return console.error('error retrieving settings for device', err);
 
-				if (settings.zw_node_id.indexOf('.') < 0 && !pollInterval.hasOwnProperty(token)) {
-					pollInterval[token] = {};
+				if (settings.zw_node_id.indexOf('.') < 0 && !endpointInterval.hasOwnProperty(token)) {
+					endpointInterval[token] = {};
 					for(var i = 1; i <= 6; i++) {
-						pollInterval[token][i] = [];
-						if (settings['poll_onoff_' + i]) setMultiInterval('onoff', i, settings['poll_onoff_' + i], token);
-						if (settings['poll_measure_' + i]) setMultiInterval('measure', i, settings['poll_measure_' + i], token);
-						if (settings['poll_meter_' + i]) setMultiInterval('meter', i, settings['poll_meter_' + i], token);
+						endpointInterval[token][i] = [];
+						if (settings['endpoint_interval_onoff_' + i]) setEndpointInterval('onoff', i, settings['endpoint_interval_onoff_' + i], token);
+						if (settings['endpoint_interval_measure_' + i]) setEndpointInterval('measure', i, settings['endpoint_interval_measure_' + i], token);
+						if (settings['endpoint_interval_meter_' + i]) setEndpointInterval('meter', i, settings['endpoint_interval_meter_' + i], token);
 					}
 				}
 			});
@@ -93,33 +93,31 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			size: 1,
 			signed: false,
 		},
-		poll_onoff_1: (newValue, oldValue, deviceData) => setMultiInterval('onoff', 1, newValue, deviceData.token),
-		poll_measure_1: (newValue, oldValue, deviceData) => setMultiInterval('measure', 1, newValue, deviceData.token),
-		poll_meter_1: (newValue, oldValue, deviceData) => setMultiInterval('meter', 1, newValue, deviceData.token),
-		poll_onoff_2: (newValue, oldValue, deviceData) => setMultiInterval('onoff', 2, newValue, deviceData.token),
-		poll_measure_2: (newValue, oldValue, deviceData) => setMultiInterval('measure', 2, newValue, deviceData.token),
-		poll_meter_2: (newValue, oldValue, deviceData) => setMultiInterval('meter', 1, newValue, deviceData.token),
-		poll_onoff_3: (newValue, oldValue, deviceData) => setMultiInterval('onoff', 3, newValue, deviceData.token),
-		poll_measure_3: (newValue, oldValue, deviceData) => setMultiInterval('measure', 3, newValue, deviceData.token),
-		poll_meter_3: (newValue, oldValue, deviceData) => setMultiInterval('meter', 3, newValue, deviceData.token),
-		poll_onoff_4: (newValue, oldValue, deviceData) => setMultiInterval('onoff', 4, newValue, deviceData.token),
-		poll_measure_4: (newValue, oldValue, deviceData) => setMultiInterval('measure', 4, newValue, deviceData.token),
-		poll_meter_4: (newValue, oldValue, deviceData) => setMultiInterval('meter', 4, newValue, deviceData.token),
-		poll_onoff_5: (newValue, oldValue, deviceData) => setMultiInterval('onoff', 5, newValue, deviceData.token),
-		poll_measure_5: (newValue, oldValue, deviceData) => setMultiInterval('measure', 5, newValue, deviceData.token),
-		poll_meter_5: (newValue, oldValue, deviceData) => setMultiInterval('meter', 5, newValue, deviceData.token),
-		poll_onoff_6: (newValue, oldValue, deviceData) => setMultiInterval('onoff', 6, newValue, deviceData.token),
-		poll_measure_6: (newValue, oldValue, deviceData) => setMultiInterval('measure', 6, newValue, deviceData.token),
-		poll_meter_6: (newValue, oldValue, deviceData) => setMultiInterval('meter', 6, newValue, deviceData.token),
+		endpoint_interval_onoff_1: (newValue, oldValue, deviceData) => setEndpointInterval('onoff', 1, newValue, deviceData.token),
+		endpoint_interval_measure_1: (newValue, oldValue, deviceData) => setEndpointInterval('measure', 1, newValue, deviceData.token),
+		endpoint_interval_meter_1: (newValue, oldValue, deviceData) => setEndpointInterval('meter', 1, newValue, deviceData.token),
+		endpoint_interval_onoff_2: (newValue, oldValue, deviceData) => setEndpointInterval('onoff', 2, newValue, deviceData.token),
+		endpoint_interval_measure_2: (newValue, oldValue, deviceData) => setEndpointInterval('measure', 2, newValue, deviceData.token),
+		endpoint_interval_meter_2: (newValue, oldValue, deviceData) => setEndpointInterval('meter', 1, newValue, deviceData.token),
+		endpoint_interval_onoff_3: (newValue, oldValue, deviceData) => setEndpointInterval('onoff', 3, newValue, deviceData.token),
+		endpoint_interval_measure_3: (newValue, oldValue, deviceData) => setEndpointInterval('measure', 3, newValue, deviceData.token),
+		endpoint_interval_meter_3: (newValue, oldValue, deviceData) => setEndpointInterval('meter', 3, newValue, deviceData.token),
+		endpoint_interval_onoff_4: (newValue, oldValue, deviceData) => setEndpointInterval('onoff', 4, newValue, deviceData.token),
+		endpoint_interval_measure_4: (newValue, oldValue, deviceData) => setEndpointInterval('measure', 4, newValue, deviceData.token),
+		endpoint_interval_meter_4: (newValue, oldValue, deviceData) => setEndpointInterval('meter', 4, newValue, deviceData.token),
+		endpoint_interval_onoff_5: (newValue, oldValue, deviceData) => setEndpointInterval('onoff', 5, newValue, deviceData.token),
+		endpoint_interval_measure_5: (newValue, oldValue, deviceData) => setEndpointInterval('measure', 5, newValue, deviceData.token),
+		endpoint_interval_meter_5: (newValue, oldValue, deviceData) => setEndpointInterval('meter', 5, newValue, deviceData.token),
+		endpoint_interval_onoff_6: (newValue, oldValue, deviceData) => setEndpointInterval('onoff', 6, newValue, deviceData.token),
+		endpoint_interval_measure_6: (newValue, oldValue, deviceData) => setEndpointInterval('measure', 6, newValue, deviceData.token),
+		endpoint_interval_meter_6: (newValue, oldValue, deviceData) => setEndpointInterval('meter', 6, newValue, deviceData.token),
 	},
 });
 
 Homey.manager('flow').on('action.PN6_reset_meter', (callback, args) => {
 	const node = module.exports.nodes[args.device.token];
-	if (node &&
-		node.instance &&
-		node.instance.CommandClass &&
-		node.instance.CommandClass.COMMAND_CLASS_METER) {
+
+	if (typeof node.instance.CommandClass.COMMAND_CLASS_METER !== 'undefined') {
 		node.instance.CommandClass.COMMAND_CLASS_METER.METER_RESET({}, (err, result) => {
 			if (err) return callback(err);
 			if (result === 'TRANSMIT_COMPLETE_OK') return callback(null, true);
@@ -128,12 +126,12 @@ Homey.manager('flow').on('action.PN6_reset_meter', (callback, args) => {
 	} else return callback('unknown_error');
 });
 
-function setMultiInterval (capability, multiChannel, value, token) {
+function setEndpointInterval (capability, multiChannel, value, token) {
 	const node = module.exports.nodes[token];
 
-	if (pollInterval[token][multiChannel][capability]) {
-		clearInterval(pollInterval[token][multiChannel][capability]);
-		pollInterval[token][multiChannel][capability] = null
+	if (endpointInterval[token][multiChannel][capability]) {
+		clearInterval(endpointInterval[token][multiChannel][capability]);
+		endpointInterval[token][multiChannel][capability] = null
 	}
 
 	if (value === 0) return;
@@ -141,8 +139,8 @@ function setMultiInterval (capability, multiChannel, value, token) {
 	switch (capability) {
 		case 'onoff': {
 			if (typeof node.instance.MultiChannelNodes[multiChannel].CommandClass.COMMAND_CLASS_SWITCH_BINARY !== "undefined") {
-				pollInterval[token][multiChannel].onoff = setInterval(() => {
-					module.exports._debug('polling: [' + multiChannel + '].' + capability);
+				endpointInterval[token][multiChannel].onoff = setInterval(() => {
+					module.exports._debug('polling: MultiChannelNode[' + multiChannel + '].' + capability);
 					node.instance.MultiChannelNodes[multiChannel].CommandClass.COMMAND_CLASS_SWITCH_BINARY.SWITCH_BINARY_GET({});
 				}, value * 1000);
 			}
@@ -150,8 +148,8 @@ function setMultiInterval (capability, multiChannel, value, token) {
 
 		case 'measure': {
 			if (typeof node.instance.MultiChannelNodes[multiChannel].CommandClass.COMMAND_CLASS_METER !== "undefined") {
-				pollInterval[token][multiChannel].measure = setInterval(() => {
-					module.exports._debug('polling: [' + multiChannel + '].' + capability);
+				endpointInterval[token][multiChannel].measure = setInterval(() => {
+					module.exports._debug('polling: MultiChannelNode[' + multiChannel + '].' + capability);
 					node.instance.MultiChannelNodes[multiChannel].CommandClass.COMMAND_CLASS_METER.METER_GET({
 						Properties1: {
 							Scale: 2,
@@ -163,8 +161,8 @@ function setMultiInterval (capability, multiChannel, value, token) {
 
 		case 'meter': {
 			if (typeof node.instance.MultiChannelNodes[multiChannel].CommandClass.COMMAND_CLASS_METER !== "undefined") {
-				pollInterval[token][multiChannel].meter = setInterval(() => {
-					module.exports._debug('polling: [' + multiChannel + '].' + capability);
+				endpointInterval[token][multiChannel].meter = setInterval(() => {
+					module.exports._debug('polling: MultiChannelNode[' + multiChannel + '].' + capability);
 					node.instance.MultiChannelNodes[multiChannel].CommandClass.COMMAND_CLASS_METER.METER_GET({
 						Properties1: {
 							Scale: 0,
